707f9fee6f2bbb11b35517da6172ba120c63580b
Nathan Long <nathanmlong@gmail.com>
2014-02-26 09:21:20 -0500

    Moving towards using a priority queue

--- lib/mendel/combiner.rb
+++ lib/mendel/combiner.rb
@@ -1,29 +1,63 @@
 require "mendel/version"
+require "priority_queue"
 
 module Mendel
-  require_relative 'smarandache'
 
   class Combiner
-    attr_accessor :list1, :list2
+    attr_accessor :lists, :priority_queue
 
-    def initialize(list1, list2)
-      self.list1   = list1
-      self.list2   = list2
+    def initialize(*lists, priority_queue, &block)
+      self.lists          = lists
+      self.priority_queue = priority_queue
+    end
+
+    # Override on instance if you like
+    def score_combinations(*items)
+      items.reduce(0) { |sum, item| sum += item }
     end
 
     def results
-      results = []
-      enum1 = Smarandache::Decrescendo.new(list1)
-      enum2 = Smarandache::Crescendo.new(list2)
-      loop do
-        a = enum1.next
-        b = enum2.next
-        results << [a, b, a + b]
-      end
-
-      results
     rescue StopIteration
       return results
+    end
+
+    # Increments which are valid for instance's lists
+    def next_steps_from(coordinates)
+      increments_from(coordinates).select { |coords| valid_for_lists?(coords, lists) }
+    end
+
+    # All possible coordinates which are one greater than the given
+    # coords in a single direction.
+    # Eg:
+    # next_steps_from([0,0]) 
+    #   #=> [[0,1], [1, 0]]
+    # next_steps_from([10,5,7])
+    #   => [[11, 5, 7], [10, 6, 7], [10, 5, 8]]
+    def increments_from(coordinates)
+      coordinates.length.times.map { |i| coordinates.dup.tap { |c| c[i] += 1} } # now we're really having fun!
+    end
+
+    # Do the coordinates represent a valid location given these lists?
+    # Eg:
+    #   valid_for_lists?([0,1], [['thundercats', 'voltron'], ['hi', 'ho']])
+    #     #=> true - represents ['thundercats', 'ho']
+    #   valid_for_lists?([0,2], [['thundercats', 'voltron'], ['hi', 'ho']])
+    #     #=> false - first list has an index 0, but second list has no index 2
+    #   valid_for_lists?([0,2,0], [['thundercats', 'voltron'], ['hi', 'ho']])
+    #     #=> false - there are only two lists
+    def valid_for_lists?(coords, lists)
+      # Must give exactly one index per list
+      return false unless coords.length == lists.length
+      coords.each_with_index.all? { |value, index| valid_index_in?(lists[index], value) }
+    end
+
+    # Eg:
+    #   valid_index_in?(['hi', 'ho'],  1) #=> true
+    #   valid_index_in?(['hi', 'ho'],  2) #=> false
+    #   valid_index_in?(['hi', 'ho'], -2) #=> true
+    #   valid_index_in?(['hi', 'ho'], -3) #=> true
+    def valid_index_in?(array, index)
+      index <= (array.length - 1) && index >= (0 - array.length)
     end
 
   end
--- spec/combiner_spec.rb
+++ spec/combiner_spec.rb
@@ -4,8 +4,43 @@ require_relative "fixtures/example_input"
 
 describe Mendel::Combiner do
 
-  let(:combiner) { Mendel::Combiner.new(list1, list2) }
+  # list1: {name: 'Jimmy', age: 10}, {name: 'Roger', age: 12}
+  # list2: {name: 'Susan', age: 8}, {name: 'Lisa', age: 14}
+  #
+  # combinations:
+  #   [
+  #     # No particular order...
+  #     {items: [{name: 'Jimmy', age: 10}, {name: 'Susan', age: 8}], score: 18}
+  #     {items: [{name: 'Jimmy', age: 10}, {name: 'Lisa', age: 14}], score: 24}
+  #     ...
+  #   ]
+
+  let(:combiner_class) {
+    Class.new do
+      def score_combination(*items)
+        items.reduce(0) { |sum, item| sum += item['age'].to_i }
+      end
+    end
+    Mendel::Combiner
+  }
+
+  let(:combiner) {
+    Mendel::Combiner.new(list1, list2, PriorityQueue.new).tap { |c|
+      # def c.score_combinations(*items)
+      #   items.reduce(0) { |sum, item| sum += item['age'].to_i }
+      # end
+    }
+  }
+
   let(:list1) { EXAMPLE_INPUT[:incrementing_integers] }
+
+  it "makes coords rightly" do
+    c = Mendel::Combiner.new(%w[a b], %w[c d], PriorityQueue.new)
+    expect(c.increments_from([0,0])).to eq([[1,0], [0,1]])
+
+    c = Mendel::Combiner.new(%w[a b], %w[c d], %w[e f], PriorityQueue.new)
+    expect(c.increments_from([0,0,0])).to eq([[1,0,0],[0,1,0],[0,0,1]])
+  end
 
   describe "when both lists increment smoothly" do
     let(:list2) { EXAMPLE_INPUT[:incrementing_decimals] }
