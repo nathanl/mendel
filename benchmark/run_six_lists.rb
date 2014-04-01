7cc531beee713c1aa60f5fd6d382d9d868172feb
Nathan Long <nathanmlong@gmail.com>
2014-04-01 15:12:29 -0400

    Second benchmarking script for 6 lists

---
+++ benchmark/run_six_lists.rb
@@ -0,0 +1,17 @@
+require_relative 'benchmarker'
+require_relative 'basic_combiner'
+
+chunk_size   = ENV.fetch('CS', 25)
+puts "Using chunk size #{chunk_size} - set ENV var CS to change"
+
+lists = 6.times.map {
+  # More than this eats a ton of memory
+  8.times.map { rand(1.0...1_000.0) }.sort
+}
+
+# require 'pry'
+# binding.pry
+# exit
+
+benchmarker = Mendel::Benchmarker.new(BasicCombiner.new(*lists), chunk_size)
+benchmarker.go!
--- benchmark/run_two_lists.rb
+++ benchmark/run_two_lists.rb
@@ -7,8 +7,8 @@ chunk_size   = ENV.fetch('CS', 10)
 puts "Using list lengths #{list1_length} and #{list2_length} and chunk size #{chunk_size}"
 puts "Set ENV vars L1, L2, and CS to change"
 
-list1    = list1_length.times.map  { rand(1_000_000)        }.sort
-list2    = list2_length.times.map  { rand(1_000_000) / 10.0 }.sort
+list1    = list1_length.times.map  { rand(1_000_000)       }.sort
+list2    = list2_length.times.map  { rand(1.0...1_000_000) }.sort
 
 benchmarker = Mendel::Benchmarker.new(BasicCombiner.new(list1, list2), chunk_size)
 benchmarker.go!
