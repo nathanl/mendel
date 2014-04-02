9157fa6ad10cbb933e6834804f1d4bb624efaf08
Nathan Long <nathanmlong@gmail.com>
2014-04-02 11:01:40 -0400

    Can build empty ASCII grid

--- lib/mendel/visualizers/ascii.rb
+++ lib/mendel/visualizers/ascii.rb
@@ -3,6 +3,33 @@ require_relative 'base'
 module Mendel
   module Visualizers
     class ASCII < Base
+
+      def initialize(*args)
+        super
+        @label_length = 8
+      end
+
+      def axis_label_for(list_item)
+        list_item.to_s.rjust(label_length, ' ')[0...label_length]
+      end
+
+      def output
+        output_lines = []
+        grid.each_with_index do |line, index|
+          x_label = axis_label_for(list1[index])
+          line = "#{x_label}" # add grid line
+          output_lines.unshift(line)
+        end
+        y_labels = list2.map {|item| axis_label_for(item) }
+        footer_line = y_labels.join('  ')
+        output_lines = output_lines.join("\n\n\n")
+        output_lines.concat("\n#{axis_label_for('')}#{footer_line}\n")
+      end
+
+      private
+
+      attr_reader :label_length
+
     end
   end
 end
--- spec/mendel/visualizers/ascii_spec.rb
+++ spec/mendel/visualizers/ascii_spec.rb
@@ -1,51 +1,85 @@
 require_relative "../../spec_helper"
+require "mendel"
 require "mendel/visualizers/ascii"
 
 describe Mendel::Visualizers::ASCII do
 
   let(:klass) { described_class }
+  let(:combiner_class) {
+    Class.new do
+      include Mendel::Combiner
+      def score_combination(items)
+        items.reduce(0) { |sum, item| sum += item }
+      end
+    end
+  }
+
+  let(:combiner)   { combiner_class.new(list1, list2) }
+  let(:list1)      { (1..10).to_a }
+  let(:list2)      { (1..10).map { |i| i + 0.1} }
+  let(:klass)      { described_class }
+  let(:visualizer) { klass.new(combiner) }
 
   describe "after initialization" do
 
-    let(:list1)      { (1..10).to_a              }
-    let(:list2)      { (1..10).map {|i| i + 0.1} }
-    let(:visualizer) { klass.new(list1, list2)   }
-
     describe "output" do
 
+      describe "axis labels" do
+
+        it "converts the list item to a string" do
+          item = '1'
+          expect(item).to receive(:to_s).and_call_original
+          visualizer.axis_label_for(item)
+        end
+
+        it "pads the output to be 8 characters wide" do
+          item = 1
+          expect(visualizer.axis_label_for(item)).to eq(
+            '       1'
+          )
+        end
+
+        it "limits the output to 8 characters" do
+          item = '0123456789'
+          expect(visualizer.axis_label_for(item)).to eq(
+            '01234567'
+          )
+        end
+
+      end
+
       it "shows an empty grid when initialized" do
-        pending "First implement base"
         expect(visualizer.output).to eq(
           <<-WOO
-          00010
+      10
 
 
-          00009
+       9
 
 
-          00008
+       8
 
 
-          00007
+       7
 
 
-          00006
+       6
 
 
-          00005
+       5
 
 
-          00004
+       4
 
 
-          00003
+       3
 
 
-          00002
+       2
 
 
-          00001
-               0001.1  0002.1  0003.1  0004.1  0005.1  0006.1  0007.1  0008.1  0009.1  0010.1
+       1
+             1.1       2.1       3.1       4.1       5.1       6.1       7.1       8.1       9.1      10.1
           WOO
         )
       end
